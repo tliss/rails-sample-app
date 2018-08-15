7431d37c75faa864f62fbb5de56fc26d0799f85e
tliss <tayloraliss@gmail.com>
2018-08-15 02:28:13 -0400

    Add missing length validation for user name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   #This is the same as validates(:name, {presence: true})
-  validates :name, presence: true
-
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
 end
