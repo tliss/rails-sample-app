8a138c8e12b203e44a88d9da079acc7be069d94a
tliss <tayloraliss@gmail.com>
2018-09-18 02:07:41 -0400

    Finish 9.1.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   # This is the same as self.email = self.email.downcase - which is the same as self.email = email.downcase
   before_save { email.downcase! }
-  #This is the same as validates(:name, {presence: true})
+  # This is the same as validates(:name, {presence: true})
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -15,5 +16,16 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
