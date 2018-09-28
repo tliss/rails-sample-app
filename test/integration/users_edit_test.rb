1717a5449b7bdcdf483e227446f99cb16470c140
tliss <tayloraliss@gmail.com>
2018-09-27 21:25:00 -0400

    Finish 10.1

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -19,4 +19,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert.alert-danger'
     assert_select 'div#error_explanation>ul>li', 4
   end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload                            # this reloads the user’s values from the database so we can confirm in the next 2 lines that they were successfully updated
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
 end
