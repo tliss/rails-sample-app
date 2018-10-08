5ba94bc4f95d6df073f41c0f05a30cce5f21edb5
Taylor Liss <tayloraliss@gmail.com>
2018-10-08 16:39:20 -0400

    Update test for users index

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -12,11 +12,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.where(activated: true).paginate(page: 1)
-    first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+    User.paginate(page: 1).each do |user|
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), text: user.name, count: 0
       end
     end
     assert_difference 'User.count', -1 do
@@ -28,14 +31,5 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
-  end
-
-  test "index as non-admin only shows activated users" do
-    log_in_as(@non_admin)
-    get users_path
-    all_users = User.where(activated: true).paginate(page: 1, per_page: User.count)
-    all_users.each do |user|
-      assert user.activated
-    end
   end
 end
