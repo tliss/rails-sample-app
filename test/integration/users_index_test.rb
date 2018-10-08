a3b8052b942f1651a7472e1f756a5f5b5358d781
Taylor Liss <tayloraliss@gmail.com>
2018-10-08 15:50:04 -0400

    Add test to verify on activated users are shown on users page

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -12,7 +12,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
@@ -28,5 +28,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "index as non-admin only shows activated users" do
+    log_in_as(@non_admin)
+    get users_path
+    all_users = User.where(activated: true).paginate(page: 1, per_page: User.count)
+    all_users.each do |user|
+      assert user.activated
+    end
   end
 end
